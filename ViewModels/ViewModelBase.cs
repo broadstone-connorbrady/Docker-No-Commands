cba56fe581e2c8d5f8a2de90cad81831209b2271
Connor Brady <connorbrady@codox.eu>
2021-01-30 18:17:20 +0000

    UPDATED Project to use .NET Core (Hopefully)

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace DockerNoCommands.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
